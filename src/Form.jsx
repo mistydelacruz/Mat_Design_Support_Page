8245cd3985122a791953b6d12615a82163a0ad89
Misty <misty.dela.cruz1@gmail.com>
2024-01-05 15:00:58 -1000

    Added the text field boxes for the form. Still continuing to add comments.

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,14 +1,15 @@
 /***************************************
 * Created by: Misty Dela Cruz
 * Created on: 1/2/2024
-* Last Modified: 1/4/2024
+* Last Modified: 1/5/2024
 * Updates:
-* Working on the Form and its features
-* Added the headings
-* Working on the text fields and dropdown
+* Working on the Form and its features.
+* Added the headings.
+* Added the text fields.
+* Will add the message text area.
 ***************************************/
 
-import { Box, FormControl, Select, Typography } from '@mui/material';
+import { Box, FormControl, InputLabel, MenuItem, OutlinedInput, Select, TextField, Typography } from '@mui/material';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -22,31 +23,60 @@ function Form() {
     *               HEADING                 *
     ***************************************/}
     <Box className="headings" display=''>
-        <Typography className="assist" variant='h3' sx={{color:'white'}} gutterBottom> {/*Typography element for the first heading*/}
-            Need Assistance?
+        <Typography className="assist" 
+        variant='h1' 
+        fontSize={50} 
+        fontWeight={'bold'} 
+        sx={{color:'white'}}
+        >                                            {/*Typography element for the first heading*/}
+        Need Assistance?
         </Typography>
-        <Typography className="assist" variant='subtitle1' sx={{color:'black'}}> {/*Typography element for the second heading*/}
-            Please fill out this form for help
+        <Typography className="assist-help" 
+        variant='subtitle1' 
+        sx={{color: 'white'}}
+        >                                            {/*Typography element for the second heading*/}
+        Please fill out this form for help
         </Typography>
     </Box>
-    <div>                   {/* Allows space between the HEADING and the FORM */}
-        <p>div</p>
-    </div>
+    <div></div>                                      {/* Allows space between the HEADING and the FORM */}
 
     {/***************************************
     *               FORM                    *
     ***************************************/}
-    <Box className="formdoc">                                            {/* Section for the form */}
-        <Card sx={{ minWidth: 275 }}>                                    {/* Added a card to place the form features on top */}
-            <CardContent>                                                {/* For the contents of the card */}
-                <FormControl>                                            {/* Items are place in a form layout */}
-                    <label>What can we help you with?</label>            {/* Label for issue dropdown */}
-                    <Select className='dropdown' defaultValue={10}>      {/* Issue dropdown */}
-                        <option value={10}>Connection</option>
-                        <option value={20}>Hardware</option>
+    <Box className="formdoc">                                              {/* Section for the form */}
+        <Card sx={{ minWidth: 275 }}>                                      {/* Added a card to place the form features on top */}
+            <CardContent>                                                  {/* For the contents of the card */}
+            {/***** DROP DOWN MENU *****/}
+                <FormControl className="select">                           {/* Components for the form are placed within the FormControl */}
+                    <label>What can we help you with?</label>              {/* Label for issue dropdown */}
+                    <Select className="dropdown" defaultValue={10}>        {/* User chooses an issue using the dropdown with menu items */}
+                        <MenuItem disabled value={10} 
+                        className="placeholder"
+                        >
+                        Please select an issue
+                        </MenuItem>
+                        <MenuItem value={20}>Connection Issue</MenuItem>
+                        <MenuItem value={30}>Hardware Issue</MenuItem>
+                        <MenuItem value={40}>Subscription Issue</MenuItem>
                     </Select>
                 </FormControl>
+
+            {/***** FULL NAME TEXT FIELD *****/}
+                <FormControl className="fullname">                         {/* Components for the form are placed within the FormControl */}
+                    <label>Full Name</label>                               {/* Label for the text field */}
+                    <TextField required
+                    defaultValue={"John Doe"}/>                            {/* User enters their full name in the text field */}
+                </FormControl>
+
+            {/***** EMAIL TEXT FIELD *****/}
+                <FormControl className="email">                            {/* Components for the form are placed within the FormControl */}
+                    <label>Email Address</label>                           {/* Label for the text field */}
+                    <TextField required
+                    defaultValue={"johndoe@xyztech.com"}/>                 {/* User enters their email adress in the text field */}
+                </FormControl>
             </CardContent>
+            
+            {/***** SUBMIT BUTTON *****/}
             <CardActions>
                 <Button size="small" sx={{color: 'white', 
                 bgcolor: 'primary.dark', 
